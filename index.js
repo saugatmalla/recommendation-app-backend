100416f26ec3ea5310da01268edc0fa000d57449
saugatmalla <saugat013@gmail.com>
2022-04-03 20:26:32 -0400

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const dotenv = require('dotenv')
+const cors = require('cors')
 
 dotenv.config({
     path: './.env'
@@ -24,6 +25,7 @@ db.connect(function (error) {
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 //Routes
 const authRoutes = require('./routes/auth/auth.js')
